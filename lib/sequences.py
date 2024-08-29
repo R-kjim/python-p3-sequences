702f37c79ffbbedc17267490d7c174f94fe663af
R-kjim <rkayjim@gmail.com>
2024-08-29 15:24:25 +0300

    sequences in python

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,16 @@
 
 def print_fibonacci(length):
     pass
+    if length==0:
+        output=[]
+    elif length<=2:
+        output=[0] if length==1 else [0,1]
+    elif length>2:
+        output=[0,1]+[None]*(length-2)
+        len1=2
+        while len1<length:
+            output[len1]=sum(output[0:len1][-2:])
+            len1+=1
+    print(output)
+
+print_fibonacci(6)
